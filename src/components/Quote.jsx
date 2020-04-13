ee747cb79ce362b52a77b535e1728661b865f5e6
BauTancredi <bautistatancredi@hotmail.com>
2020-04-13 16:22:29 -0300

    Styled quote

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,25 +1,45 @@
 import React from "react";
+import styled from "@emotion/styled";
 
+const ResultDiv = styled.div`
+  color: #fff;
+  span {
+    font-weight: bold;
+  }
+`;
+
+const Info = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: bold;
+  }
+`;
+const Price = styled.p`
+  font-size: 30px;
+  span {
+    font-weight: bold;
+  }
+`;
 const Quote = ({ result }) => {
   if (Object.keys(result).length === 0) return null;
   return (
-    <div>
-      <p>
+    <ResultDiv>
+      <Price>
         The price is: <span>{result.PRICE}</span>
-      </p>
-      <p>
+      </Price>
+      <Info>
         Max price: <span>{result.HIGHDAY}</span>
-      </p>
-      <p>
+      </Info>
+      <Info>
         Min price: <span>{result.LOWDAY}</span>
-      </p>
-      <p>
+      </Info>
+      <Info>
         Diff last 24hs: <span>{result.CHANGEPCT24HOUR}</span>
-      </p>
-      <p>
+      </Info>
+      <Info>
         Last update: <span>{result.LASTUPDATE}</span>
-      </p>
-    </div>
+      </Info>
+    </ResultDiv>
   );
 };
 
